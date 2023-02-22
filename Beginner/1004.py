6dc88daccb4ab86fe95b8dbcd85ba3d70aeac378
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-22 09:45:05 -0800

    Create 1004.py

---
+++ Beginner/1004.py
@@ -0,0 +1,9 @@
+'''
+Read two integer values. After this, calculate the product between them and store the result in a
+variable named PROD. Print the result like the example below.
+Do not forget to print the end of line after the result, otherwise you will receive “Presentation Error”.
+'''
+a = int(input())
+b = int(input())
+product = a * b
+print("PROD =", product)
