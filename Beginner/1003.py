e8aba99b70da715a25b489a371df33ef3d7ef9d7
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-02-22 09:36:08 -0800

    Create 1003.py

---
+++ Beginner/1003.py
@@ -0,0 +1,9 @@
+'''
+Read two integer values, in this case, the variables A and B. After this,
+calculate the sum between them and assign it
+to the variable SOMA. Write the value of this variable.
+'''
+A = int(input())
+B = int(input())
+soma = A+B
+print("SOMA =", soma)
