9eba75587b571b2ebfd37ecf148714dd5a6490e0
Al Mosrof Mia <65063391+almosrof120@users.noreply.github.com>
2023-03-15 11:19:45 -0700

    Create 1007.dart

---
+++ Beginner/1007.dart
@@ -0,0 +1,23 @@
+/*
+Read four integer values named A, B, C and D. 
+Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+
+Input
+The input file contains 4 integer values.
+
+Output
+Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, 
+according to the following example, with a blank space before and after the equal signal.
+*/
+
+import 'dart:io';
+
+void main(List<String> args) {
+  int a = int.parse(stdin.readLineSync());
+  int b = int.parse(stdin.readLineSync());
+  int c = int.parse(stdin.readLineSync());
+  int d = int.parse(stdin.readLineSync());
+
+  var dif = (a * b) - (c * d);
+  print('DIFERENCA = $dif');
+}
